31ddfe42423643488074d35ea5e4eb829dd04e0c
yta-br <ytanya.br@gmail.com>
2016-09-20 17:53:31 +0300

    detete excess code

--- SiteParser/Program.cs
+++ SiteParser/Program.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Net;
 using System.IO;
 using System.Text.RegularExpressions;
-using System.Collections.Generic;
 
 namespace ConsoleApplication3
 {
@@ -61,15 +60,6 @@ namespace ConsoleApplication3
             InitSites("http://skoroto.ru/search?pcode=849069U000", "resultPrice \">\\n(\\t*)(.*) ", "resultPrice \">",
                       "http://www.7zap.ru/search.html?article=849069U000&sort___search_results_by=final_price&x=0&y=0&g=1&smode=A2&term=", @"<nobr>. ...", "<nobr>",
                       "http://www.autodoc.ru/Price/Index?Article=849069U000", "....,..</span>", "</span>");
-            /*
-            CSite skoroto = new CSite("http://skoroto.ru/search?pcode=849069U000", "resultPrice \">\\n(\\t*)(.*) ", "resultPrice \">");
-            skoroto.CheckPrice();
-            CSite Zap7 = new CSite("http://www.7zap.ru/search.html?article=849069U000&sort___search_results_by=final_price&x=0&y=0&g=1&smode=A2&term=", @"<nobr>. ...", "<nobr>");
-            Zap7.CheckPrice();
-            //CSite emex = new CSite("http://www.emex.ru/f?detailNum=849069U000&packet=-1");
-            //emex.CheckPrice();
-            CSite autodoc = new CSite("http://www.autodoc.ru/Price/Index?Article=849069U000", "....,..</span>", "</span>");
-            autodoc.CheckPrice();*/
             foreach (CSite aSite in sites)
             {
                 aSite.CheckPrice();
